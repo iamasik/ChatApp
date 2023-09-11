b28373a250fbf3f691e187c5267325ebad5e8deb
DH Asik <delwarhossenasik@gmail.com>
2023-09-11 10:11:06 +0600

    Updated Intial setup

---
+++ app.js
@@ -0,0 +1,16 @@
+// _________Initial setup_______
+const express=require("express")
+const app=express()
+app.use(express.json())
+
+const morgan = require("morgan")
+app.use(morgan('dev'))
+
+// https://nodejs.org/api/path.html#path_path_resolve_paths
+const path=require('path')
+
+const cookieParser=require('cookie-parser')
+app.use(cookieParser())
+//__________Initial route_______
+
+exports.app=app
