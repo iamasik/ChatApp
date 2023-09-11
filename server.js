5c2b6c9d922a118b021b9bd59bd8ba38b94d8fc3
DH Asik <delwarhossenasik@gmail.com>
2023-09-11 16:45:00 +0600

    Sever setup Done

--- server.js
+++ server.js
@@ -1,16 +1,27 @@
-const app=require('./app')
+const StartApp=require('./app')
 const mongoose= require('mongoose')
 
 const dotenv=require('dotenv')
 dotenv.config({path:'./config.env'})
 
-const DB="mongodb+srv://dhasikdevApp:dcsp1dJz4QXVfE5M@cluster0.rprjf2d.mongodb.net/?retryWrites=true&w=majority"
+const DB=process.env.Database.replace("<password>",process.env.Password)
 mongoose.set("strictQuery", false);
 
-const Connection= function() {
-    mongoose.connect(DB,{
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    })
-    print("COnnected")
+async function Connection() {
+    try{
+        await mongoose.connect(DB,{
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        })
+        console.log("DataBase Connected");
+    }
+    catch(err){
+        console.log(`Connection failed: ${err.message}`);
+        process.exit(1)
+    }
 }
+Connection()
+
+const Server=StartApp.app.listen(process.env.Port,()=>{
+    console.log("Server Started")
+})
